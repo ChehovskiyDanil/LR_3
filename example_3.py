0faad21ab31de271c0dab9d9dcceae3fb88d0837
chehpvskiy01@bk.ru <chehovskiy01@bk.ru>
2025-11-16 22:48:25 +0300

    Add example_3.py

---
+++ example_3.py
@@ -0,0 +1,12 @@
+import  math
+
+if __name__ == '__main__':
+    n = int(input("value of n? "))
+    x = float(input("value of x? "))
+
+    s = 0.0
+    for k in range(1, n + 1):
+        a = math.log(k * x) / (k * k)
+        s += a
+
+    print(f's = {s}')
